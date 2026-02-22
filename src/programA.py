3a0cc7bdcd0d5f6b727503ca16b3e76d401cadfe
Azka <05azka05@gmail.com>
2026-02-22 15:56:28 +0000

    main menu started

--- src/programA.py
+++ src/programA.py
@@ -1,3 +1,11 @@
+def show_menu():
+    print("Welcome to the Maths Converter!")
+    print("Please select an option:")
+    print("1) Convert Decimal to Hexadecimal (16-bit)")
+    print("2) Little-endian pack/unpack (16-bit)")
+    print("3) ASCII memory dump")
+    print("4) Array addressing")
+    print("5) Stack frame")
+    print("0) Exit")
+
 def convert(n):
-    pass
-    #sample code just for folder
