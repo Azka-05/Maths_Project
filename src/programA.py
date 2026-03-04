e8b413d43a59aa99919b392e2ce2b068e2694802
Azka <05azka05@gmail.com>
2026-03-04 11:48:25 +0000

    update

--- src/programA.py
+++ src/programA.py
@@ -1,13 +1,3 @@
-def show_menu():
-    print("Welcome to the Maths Converter!")
-    print("Please select an option:")
-    print("1) Convert Decimal to Hexadecimal (16-bit)")
-    print("2) Little-endian pack/unpack (16-bit)")
-    print("3) ASCII memory dump")
-    print("4) Array addressing")
-    print("5) Stack frame")
-    print("0) Exit")
-
 def convert(n):
     hex_val = format(n, "X")
     bin_val = format(n, "016b")
@@ -43,5 +33,14 @@ def main():
 
 if __name__ == "__main__":    main()
 
+def show_menu():
+    print("Welcome to the Maths Converter!")
+    print("Please select an option:")
+    print("1) Convert Decimal to Hexadecimal (16-bit)")
+    print("2) Little-endian pack/unpack (16-bit)")
+    print("3) ASCII memory dump")
+    print("4) Array addressing")
+    print("5) Stack frame")
+    print("0) Exit")
 
 
