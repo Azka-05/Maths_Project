6b87f68709f835d9c5bf8afa0e627d1cee552823
HananM1 <88743860+HananM1@users.noreply.github.com>
2026-03-12 16:09:09 +0000

    comments added

--- src/option1_convert.py
+++ src/option1_convert.py
@@ -1,15 +1,23 @@
+# option1_convert.py
 # conversion from decimal to hex and binary
 
-# option 1 conversion logic
+#this function converts a decimal number into hexadecimal.
+def decimal_to_hex_16bit():  
+    try:
+        # Ask the user to enter a decimal number between 0 and 65535.
+        num = int(input("Enter a decimal number (0 - 65535): "))
 
-def decimal_to_hex_16bit(n):
+        # Check if the number is outside the valid range for a 16-bit unsigned integer.
+        if num < 0 or num > 65535:
+            print("Number out of range for 16-bit unsigned integer.")
+            return
 
-    hex_val = format(n, "X")
-    bin_val = format(n, "016b")
+        # converts a decimal number into hexadecimal.
+        # format(num, '04X') is used to convert the decimal number into 4 digit hexadecimal. 
+        hex_value = format(num, '04X')
+        #prints the hexidecimals. 
+        print("Hexadecimal (16-bit):", hex_value)
 
-    if n < 32768:
-        signed = n
-    else:
-        signed = n - 65536
-
-    return hex_val, bin_val, signed
+    # if user enters something that isnt a number then then this error message will print.
+    except ValueError:
+        print("Invalid input. Please enter a valid decimal number.")
