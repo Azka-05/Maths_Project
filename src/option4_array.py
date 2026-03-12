fce72d47a29bc1439f2a2db0c50f0269c546df2b
Azka <05azka05@gmail.com>
2026-03-12 17:20:27 +0000

    option 4 comments

--- src/option4_array.py
+++ src/option4_array.py
@@ -1,34 +1,28 @@
 # option4_array.py
-# Array Addressing + Dereference
+# array addressing calculations and simple memory read/write
 
-# simple toy memory
-memory = {}
+memory = {} #to store values at addresses for read/write operations
 
-def element_address(base, index, size):
-    """
-    Calculate address using:
-    base + index * size
-    """
-    return base + index * size
+def element_address(base, index, size): #calculate address of array element
+    return base + index * size #returns the calclated address 
 
 
-def memory_write(addr, value):
+def memory_write(addr, value):#writes a value to the specified address 
     memory[addr] = value
 
 
-def memory_read(addr):
-    return memory.get(addr, 0)
+def memory_read(addr):# reads value from memory address
+    return memory.get(addr, 0) #if address has no value it retrurns 0
 
 
-def array_addressing(base, index, size, mode, value=None):
-
+def array_addressing(base, index, size, mode, value=None): #calculates address of an array element
     address = element_address(base, index, size)
-    print(f"ADDRESS = base+ index*size = {address}")
+    print(f"ADDRESS = base+ index*size = {address}") #displays the address calculation
 
-    if mode == "write":   # lowercase
-        memory_write(address, value)
+    if mode == "write":   # checks if the operation is write
+        memory_write(address, value)#writes value to memory
         print(f"WRITE size={size} value={value} to ADDRESS {hex(address)}")
 
-    elif mode == "read":
-        val = memory_read(address)
+    elif mode == "read": #checks if the operation is read
+        val = memory_read(address) #reads value from memory
         print(f"READ size={size} from ADDRESS {hex(address)} = {val}")
